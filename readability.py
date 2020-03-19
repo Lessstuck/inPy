23796feb40b55d82fa4e0e20e3f2d5584f6f3a3e
Les Stuck <les@lesstuck.com>
2020-03-19 09:36:05 -0600

    submitted version

--- readability.py
+++ readability.py
@@ -4,14 +4,11 @@ from cs50 import get_string
 def main():
     s = get_string("Text: ")
     #  from https://www.geeksforgeeks.org/python-string-length-len
-    letter_count = len(s)
-    print(f"{letter_count} letters")
+    letter_count = count_letters(s)
     # from https://www.geeksforgeeks.org/python-program-to-count-words-in-a
     word_count = len(s.split())
-    print(f"{word_count} words")
     #  from https://stackoverflow.com/questions/29166774/count-number-of-sentences-in-paragraph
     sentence_count = count_sentences(s)
-    print(f"{sentence_count} sentence(s)")
     index = coleman_liau(letter_count, word_count, sentence_count)
     # print messages for different index values
     if (index < 1):
@@ -26,12 +23,26 @@ def main():
 
 def coleman_liau(letter_count, word_count, sentence_count):
     L = (letter_count / (word_count / 100.))
-    print(f"L: {L}")
     S = (sentence_count / (word_count / 100.))
-    print(f"S: {S}")
-    index = (L * 0.0588) - (S * 0.296) - 15.8
-    print(f"index: {index}")
+    index = round((L * 0.0588) - (S * 0.296) - 15.8)
     return (index)
+
+
+# counts letters in string
+def count_letters(s):
+    i = 0
+    letters = 0
+    while True:
+        #  count upper-case letters
+        if (ord(s[i]) >= 65 and ord(s[i]) <= 90):
+            letters += 1
+        # count lower-case letters
+        elif (ord(s[i]) >= 97 and ord(s[i]) <= 122):
+            letters += 1
+        i += 1
+        if (i == len(s)):
+            break
+    return letters
 
 
 #  counts periods, question marks, and exclamation marks

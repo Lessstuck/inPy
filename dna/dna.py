ab3fafd2a5f68e5189f03f035e0aef6cf5850c17
Les Stuck <les@lesstuck.com>
2020-03-30 00:38:43 -0600

    better variable names, new STR counting framework

--- dna/dna.py
+++ dna/dna.py
@@ -1,6 +1,7 @@
 from cs50 import sys
 import csv
 
+# check number of arguments
 if (len(sys.argv) <= 2) or (len(sys.argv) > 3):
     print("Usage: python dna.py data.csv sequence.txts")
     exit(1)
@@ -11,22 +12,29 @@ dnaFileName = sys.argv[2]
 # from https://docs.python.org/3/library/csv.html
 with open(dbFileName, newline='') as db:
     reader = csv.DictReader(db)
-    # for row in reader:
-        # print(row['name'], row['AGATC'])
-
-# print(f"database: {dbFileName}")
+    # from Tyler at https://stackoverflow.com/questions/24962908/how-can-i-read-only-the-header-column-of-a-csv-file-using-python
+    fieldNames = reader.fieldnames
+    fieldNames.remove('name')
+    print(fieldNames)
 
 # open DNA sequence file
-with open(dnaFileName, "r") as seq:
-    while True:
-        name = seq.read()
-        if not name:
-            # End of File
-            break
-        # print(f"item: {name} length: {len(name)}")
-    # parse sequence
-    for i in range(0, (len(name) - 4)):
-        nucleoByte = name[i:(i + 4)]
-        print(f"nucleoByte: {nucleoByte}")
+with open(dnaFileName, "r") as sequence:
+    seq = sequence.read()
+
+# parse sequence
+repeatsMax = []
+for i in range(0, (len(fieldNames))):
+    repeatsMax.append(0)
+    repeats = 0
+    STR = fieldNames[i]
+    lenSTR = len(STR)
+    for j in range(0, (len(seq) - lenSTR)):  # counts repeats, not contiguous repeats
+        if STR == seq[j:(j + lenSTR)]:
+            repeats += 1
+        repeatsMax[i] = max(repeatsMax[i], repeats)
+
+print(repeatsMax)
+
+# TODO compare repeatsMax with each person's STR pattern
 
 exit(0)
